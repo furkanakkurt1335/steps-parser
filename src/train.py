1db9acd131efedfe9ae685e2e02f25804ab118de
Furkan Akkurt <furkanakkurt9285@gmail.com>
2023-01-11 11:00:43 +0000

    add pretty eval results to output

--- src/train.py
+++ src/train.py
@@ -38,6 +38,7 @@ def main(config, eval_mode="basic"):
 
     if "test" in config["data_loaders"]["paths"]:
         eval_results = evaluate_best_trained_model(trainer, config, eval_mode=eval_mode)
+    print_eval_results(config['name'], eval_results)
 
     pth_l = [i for i in os.listdir(str(config._save_dir)) if i.endswith('.pth')]
     for pth_t in pth_l:
@@ -47,6 +48,25 @@ def main(config, eval_mode="basic"):
 
     send_email(config['name'], config['experiment'], os.environ.get('SLURM_JOB_ID'), eval_results)
 
+
+def print_eval_results(train_type, eval_results):
+    if train_type == 'feats-only':
+        res = 'UFeats: {ufeats}'.format(ufeats=eval_results['UFeats'].f1)
+    elif train_type == 'lemma-only':
+        res = 'Lemmas: {lemmas}'.format(lemmas=eval_results['Lemmas'].f1)
+    elif train_type == 'pos-only':
+        res = 'UPOS: {upos}'.format(upos=eval_results['UPOS'].f1)
+    elif train_type == 'dep-parsing':
+        res = 'UAS: {uas}, LAS: {las}'.format(uas=eval_results['UAS'].f1, las=eval_results['LAS'].f1)
+    elif train_type == 'dep-parsing_upos':
+        res = 'UAS: {uas}, LAS: {las}'.format(uas=eval_results['UAS'].f1, las=eval_results['LAS'].f1)
+    elif train_type == 'dep-parsing_feats':
+        res = 'UAS: {uas}, LAS: {las}'.format(uas=eval_results['UAS'].f1, las=eval_results['LAS'].f1)
+    elif train_type == 'dep-parsing_upos_feats':
+        res = 'UAS: {uas}, LAS: {las}'.format(uas=eval_results['UAS'].f1, las=eval_results['LAS'].f1)
+    elif train_type == 'upos_feats':
+        res = 'UFeats: {ufeats}'.format(ufeats=eval_results['UFeats'].f1)
+    print('Eval results: {}.'.format(res))
 
 def evaluate_best_trained_model(trainer, config, eval_mode="basic"):
     """Evaluate the model with best validation performance on test data after training.
